cad60b56e898364ba01bd66bb0c941c3cc02f8cf
KL <kinkihyunsassy@gmail.com>
2019-09-12 20:24:05 +0800

    first commit

---
+++ BoostTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BoostTest
+//
+//  Created by Ong Kah Lam on 12/09/2019.
+//  Copyright © 2019 Ong Kah Lam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
